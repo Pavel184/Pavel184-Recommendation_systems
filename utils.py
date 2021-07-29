f68eb969713f8fd69ab34c5de762cd691fe905c0
Pavel Krupenya <pavelkrupenja@rambler.ru>
2021-07-29 22:54:30 +0200

    Homework_lesson_6

---
+++ utils.py
@@ -0,0 +1,31 @@
+"""
+Utils
+
+"""
+import numpy as np
+
+
+def prefilter_items(data, item_features, take_n_popular):
+    # Уберем самые популярные товары (их и так купят)
+    popularity = data.groupby('item_id')['user_id'].nunique().reset_index() / data['user_id'].nunique()
+    popularity.rename(columns={'user_id': 'share_unique_users'}, inplace=True)
+    
+    top_popular = popularity[popularity['share_unique_users'] > 0.5].item_id.tolist()
+    data = data[~data['item_id'].isin(top_popular)]
+    
+    # top_5000 = popularity.sort_values('n_sold', ascending=False).head(take_n_popular).item_id.tolist() # как вариант топ-N
+    
+    # Уберем самые НЕ популярные товары (их и так НЕ купят)
+    top_notpopular = popularity[popularity['share_unique_users'] < 0.01].item_id.tolist()
+    data = data[~data['item_id'].isin(top_notpopular)]
+    
+    # Уберем товары, которые не продавались за последние 12 месяцев
+    
+    # Уберем не интересные для рекоммендаций категории (department)
+    
+    # Уберем слишком дешевые товары (на них не заработаем). 1 покупка из рассылок стоит 60 руб. 
+    
+    # Уберем слишком дорогие товарыs
+    
+    # ...
+    return data
